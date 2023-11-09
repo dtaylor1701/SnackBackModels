7beda978f2cb94cd4652be08e7b5134c36a4ca10
David Taylor <dktaylor1701@gmail.com>
2023-11-09 00:21:00 -0500

    added public inits

--- Sources/SnackBackModels/FeedbackContent.swift
+++ Sources/SnackBackModels/FeedbackContent.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public struct FeedbackContent: Codable {
   public let message: String
+
+  public init(message: String) {
+    self.message = message
+  }
 }
--- Sources/SnackBackModels/FeedbackRequest.swift
+++ Sources/SnackBackModels/FeedbackRequest.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct FeedbackRequest: Codable {
   public let destinationEmail: String
   public let content: FeedbackContent
+
+  public init(destinationEmail: String, content: FeedbackContent) {
+    self.destinationEmail = destinationEmail
+    self.content = content
+  }
 }
